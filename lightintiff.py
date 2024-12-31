cdc33961624942d27aea66e964f98a3ed3e03a7f
yuyuange <116905182+yuyuange@users.noreply.github.com>
2024-12-31 17:27:12 +0800

    Update lightintiff.py

--- lightintiff.py
+++ lightintiff.py
@@ -12,10 +12,14 @@ import cv2
 import numpy as np
 import pandas as pd
 
-
-n_photo = 110870
+series = '030'
+n_photo = 115211
 path = os.path.dirname(__file__) 
-img = cv2.imread(path+'/image/iss030e%d.JPG'%n_photo,-1)
+
+img_path = os.path.join(path, series, 'image', 'iss' + series + 'e%d.JPG' % n_photo)
+if os.name == 'nt':  # Windows
+    img_path = img_path.replace('/', '\\')
+img = cv2.imread(img_path,-1)
 
 xc_list = []
 yc_list = []
@@ -25,7 +29,7 @@ count = 0
 def on_EVENT_RBUTTONDOWN(event, x, y, flags, param):
     global xc,yc,count,img
     if event == cv2.EVENT_LBUTTONDOWN:
-        img = cv2.imread(path+'/image/iss030e%d.JPG'%n_photo,-1)
+        img = cv2.imread(img_path,-1)
         xy = "  %d,%d" % (x, y)
         cv2.circle(img, (x, y), 20, (255, 255, 255), thickness = 3)
         cv2.putText(img, xy, (x, y), cv2.FONT_HERSHEY_PLAIN,
@@ -49,18 +53,26 @@ while(1):
     # else if cv2.waitKey(0)&0xFF==:
         
 cv2.destroyAllWindows()
-cv2.waitKey(1)
+cv2.waitKey(0)
+# while True:
+#     key = cv2.waitKey(1) & 0xFF
+#     if key == ord("q"):  # 按下 'q' 键退出循环
+#         break
+
+# # 关闭窗口
+# cv2.destroyWindow("image")
+
 
 # position = pd.read_table("/Users/yuyuan/Desktop/aurora/030e51088.txt",header=None)
 # lat = position[1]
 # lon = position[2]
-city_light = pd.read_csv(path+'/030e115216.csv')
+city_light = pd.read_csv(path+'/'+series +'/'+series +'e115216.csv')
 lat = city_light['lat'].values
 lon = city_light['lon'].values
 u_cl = xc_list
 v_cl = yc_list
 city_light = pd.DataFrame({'lat':lat,'lon':lon,'u_cl':u_cl,'v_cl':v_cl})
-city_light.to_csv(path+"/030e%d.csv"%n_photo)
+city_light.to_csv(path+'/'+series +'/'+series +"e%dcitylights.csv"%n_photo)
 
 # -*- coding: utf-8 -*-
 """
